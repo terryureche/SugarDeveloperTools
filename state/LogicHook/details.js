0db2193b0b9e580b9dd55724a9562ece94336007
terryureche <terryureche@gmail.com>
2019-11-04 23:06:05 +0200

    handle the class for next action on input

--- state/LogicHook/details.js
+++ state/LogicHook/details.js
@@ -1,13 +1,22 @@
 let InputComponent = require("./../../actions/InputComponent")["inputComponent"];
+let LogicHook = require("./../../SugarComponents/LogicHook/LogicHook");
+let chalk = require("chalk");
+
 
 let logicHookType = (value) => {
     return {
         type: "input",
         value: value,
+        callback: () => {
+            console.log(chalk.green.bgBlack.bold(`Please enter data for ${value}`));
+
+        },
         class: InputComponent,
         constructParams: [logicHookInputs
         ],
-        functionParams: []
+        functionParams: [{
+            default: LogicHook,
+        }]
     }
 }
 
@@ -45,6 +54,20 @@ const logicHookInputs = [{
             return true;
         } else {
             return "Please enter at least 5 characters"
+        }
+    }
+},
+{
+    name: "hook_module_name",
+    type: "input",
+    message: "Hook Module (Empty for global hook)",
+    validate: (value) => {
+        if (value.length > 5) {
+            return true;
+        } else {
+            console.log("This hook will be created as global hook");
+
+            return true;
         }
     }
 }];
